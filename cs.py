4ee683fd214b0762103f3e299f7846c3ca368fe6
yngkzk <ansar.iskakow@gmail.com>
2023-05-19 21:49:27 +0600

    Update cs.py
    Задания

--- cs.py
+++ cs.py
@@ -1,2 +1,18 @@
-team = "High School Seirin"
-print(team, "welcome", "to", "the", "club", sep=" ", end="!")
+users_req = input("Введите длину стороны квадрата: ")
+users_numb = int(users_req)
+result = users_numb ** 2
+print("Ваша площадь квадрата: ", result)
+
+# Вторая задача 
+
+''' 
+
+users_numb = input("Введите первое число: ")
+users_numb2 = input("Введите второе число: ")
+number = int(users_numb)
+number2 = int(users_numb2)
+result = (number + number2) / 2
+print("Ваше число: ", result)
+
+'''
+# Первая задача
