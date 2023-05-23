daa6129306e3f43ba62023cbe39bedcfdd5db871
yngkzk <ansar.iskakow@gmail.com>
2023-05-23 20:48:15 +0600

    Create cs10.py

---
+++ cs10.py
@@ -0,0 +1,9 @@
+three_digit_number = input("Enter three digit number: ")
+tdn = int(three_digit_number)
+n1 = tdn // 100
+n2 = tdn % 100 // 10
+n3 = tdn % 10
+sn1 = str(n1)
+sn2 = str(n2)
+sn3 = str(n3)
+print(sn3, sn2, sn1, sep="")
