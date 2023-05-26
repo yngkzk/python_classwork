49ad3bfd81b67b453217a6a0883cdd7ec4397096
yngkzk <ansar.iskakow@gmail.com>
2023-05-26 23:15:02 +0600

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,15 +1,4 @@
-users_req = input("Введите длину стороны квадрата: ")
+users_req = input("Enter the length of the side of the square: ")
 users_numb = int(users_req)
 result = users_numb ** 2
-print("Ваша площадь квадрата: ", result)
-
-# Вторая задача 
-
-# users_numb = input("Введите первое число: ")
-# users_numb2 = input("Введите второе число: ")
-# number = int(users_numb)
-# number2 = int(users_numb2)
-# result = (number + number2) / 2
-# print("Ваше число: ", result)
-
-# Первая задача
+print("Your square area: ", result)
