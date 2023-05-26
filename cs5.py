72af18aaa15cfa81b910f4a73e6ee46383db226c
yngkzk <ansar.iskakow@gmail.com>
2023-05-26 21:39:41 +0600

    classwork w some changes

--- cs5.py
+++ cs5.py
@@ -2,9 +2,13 @@ print("Enter your current time below.")
 hours = input("Hours: ")
 minutes = input("Minutes: ")
 seconds = input("Seconds: ")
-h = int(hours) * 60 * 60
-m = int(minutes) * 60
-s = int(seconds)
 day = 24 * 60 * 60
-res = day - (h + m + s)
-print("Remaining time in seconds: ", res)
+try:
+    h = int(hours) * 60 * 60
+    m = int(minutes) * 60
+    s = int(seconds)
+except ValueError as err:
+    print("Error with data", err)
+else:
+    res = day - (h + m + s)
+    print("Remaining time in seconds: ", res)
