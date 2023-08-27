6de110a4a320ccca87175569eb8fd01908a8571e
yngkzk <ansar.iskakow@gmail.com>
2023-08-27 12:15:13 +0600

    Update ex7.py

--- July-21 CW/ex7.py
+++ July-21 CW/ex7.py
@@ -1,17 +1,21 @@
 # Функция принимает двумерный массив, содержащий буквы, и выводит два слова, составленные из букв главной и побочной диагоналей.
-def get_diag_words(array=None):
-    if array:
-        word = ''
-        for r in range(0, len(array)):
-            for c in range(0, len(array[r])):
-                if r == c:
-                    word += array[r][c]
-        word += ' '
-        for c2 in range(0, len(array)):
-            index = len(array[c2]) - c2 - 1
-            word += array[c2][index]
-        return word
-    return ''
+def get_diag_words(array):
+    if not array:
+        return ''
+    word = ''
+    word2 = ''
+    for r in range(len(array)):
+        for c in range(len(array[r])):
+            if r == c:
+                word += array[r][c]
+            if c == len(array[r]) - r - 1:
+                word2 += array[r][c]
+    return word + ' ' + word2
+    # word += ' '
+    # for c2 in range(0, len(array)):
+    #     index = len(array[c2]) - c2 - 1
+    #     word += array[c2][index]
+    # return word
 
 
 def test_get_diag_words():
